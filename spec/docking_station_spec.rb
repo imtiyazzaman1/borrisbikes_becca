5eab71fbd04be1ea09ee34cf8fc80ed0e2f784a8
Raylene <smith.raylene@gmail.com>
2018-10-05 14:39:30 +0100

    Allowed doubles to respond to working method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,17 +2,20 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
+	let(:bike) { double :bike }
 	it { is_expected.to respond_to :release_bike }
 
 	it 'releases bike when requested' do
 		docking_station = DockingStation.new
-		docking_station.dock_bike(double(:bike))
-		expect(docking_station.release_bike).to be_an_instance_of(Bike)
+		allow(bike).to receive(:working?).and_return(true)
+		docking_station.dock_bike(bike)
+		expect(docking_station.release_bike).to eq bike
 	end
 
 it 'docks bikes at the docking station' do
 	docking_station = DockingStation.new
-	docking_station.dock_bike(double(:bike))
+	allow(bike).to receive(:working?).and_return(true)
+	docking_station.dock_bike(bike)
 	expect(docking_station.rack.length).to eq 1
 end
 
@@ -23,24 +26,28 @@ end
 
 it 'raises an error if trying to dock bike at a full station' do
 	docking_station = DockingStation.new
-	20.times { docking_station.dock_bike(double(:bike)) }
-	expect { docking_station.dock_bike(double(:bike))}.to raise_error('Docking station full')
+	allow(bike).to receive(:working?).and_return(true)
+	20.times { docking_station.dock_bike(bike) }
+	expect { docking_station.dock_bike(bike)}.to raise_error('Docking station full')
 end
 
 it 'lets user change capacity' do
 	docking_station = DockingStation.new(10)
-	10.times { docking_station.dock_bike(double(:bike)) }
-	expect { docking_station.dock_bike(double(:bike)) }.to raise_error('Docking station full')
+	allow(bike).to receive(:working?).and_return(true)
+	10.times { docking_station.dock_bike(bike) }
+	expect { docking_station.dock_bike(bike) }.to raise_error('Docking station full')
 end
 
 it 'allows users to report if bike is broken' do
 	docking_station = DockingStation.new
-	expect(docking_station.dock_bike(double(:bike(true))).to eq "You have reported the bike broken"
+	allow(bike).to receive(:working?).and_return(false)
+	expect(docking_station.dock_bike(bike)).to eq "You have reported the bike broken"
 end
 
 it 'refuses to release broken bikes' do
 	docking_station = DockingStation.new
-	docking_station.dock_bike(double(:bike(true)))
+	allow(bike).to receive(:working?).and_return(false)
+	docking_station.dock_bike(bike)	
 	expect { docking_station.release_bike }.to raise_error('Cannot release bike. Bike is broken')
 end
 
